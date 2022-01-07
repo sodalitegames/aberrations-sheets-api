59aeb0ffc5264403ebee5324fc031208109e1afa
Talon Durrant <talonmdurrant@gmail.com>
2022-01-07 18:46:14 -0500

    getting started with socket.io

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const { createServer } = require('http');
+const { Server } = require('socket.io');
 
 process.on('uncaughtException', err => {
   console.log(err);
@@ -15,7 +17,32 @@ mongoose.connect(process.env.DB_CONNECTION_STRING, { useNewUrlParser: true, useU
 
 const port = process.env.PORT || 2341;
 
-const server = app.listen(port, () => {
+// Set up socket.io connections
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['my-custom-header'],
+    credentials: true,
+  },
+});
+
+io.on('connection', function (socket) {
+  console.log('A user connected');
+
+  socket.on('joinRoom', roomName => {
+    socket.join(roomName);
+    socket.to(roomName).emit('message', 'A new react client has joined this room');
+    console.log('A react client has joined room:', roomName);
+  });
+
+  socket.on('disconnect', function () {
+    console.log('A user diconnected');
+  });
+});
+
+const server = httpServer.listen(port, () => {
   console.log(`Server listening on port ${port}...`);
 });
 
