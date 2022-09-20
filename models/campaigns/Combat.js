5ab56ec5be004c496c4701a76e6fbe2f99bc14cc
Talon Durrant <talonmdurrant@gmail.com>
2022-09-20 16:29:33 -0600

    updated combat model and added combatId to list

--- models/campaigns/Combat.js
+++ models/campaigns/Combat.js
@@ -7,6 +7,10 @@ const combatSchema = new mongoose.Schema(
       required: [true, 'A combat must have an associated sheetId'],
     },
     activeTurn: mongoose.Schema.ObjectId,
+    description: {
+      type: String,
+      required: [true, 'A combat must have a description'],
+    },
     combatants: {
       type: [{ name: String, type: { type: String, enum: ['players', 'npcs', 'creatures'] }, _id: mongoose.Schema.ObjectId, initiative: Number, inCombat: Boolean }],
       required: [true, 'A combat must have combatants'],
